459f50326e4947e3aafc008ca159a3b887cfe94e
Ekansh Jain <ejekanshjain@gmail.com>
2020-01-22 10:11:49 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const mysql = require('mysql')
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+const port = process.env.PORT
+
+const conn = mysql.createConnection({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'sih_ccm_2020'
+})
+conn.connect(err => {
+    if (err) throw err
+    console.log('DB connected...')
+})
+
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}...`)
+})
